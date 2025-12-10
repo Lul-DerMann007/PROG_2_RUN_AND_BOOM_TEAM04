20e3190d130925d29a3980491e4ef489a8b37660
Jonte <michaaeljontelahrs@gmail.com>
2025-12-10 13:02:21 +0100

    Jonte - Obstacle Klasse unterstützt nun verschiedenen Hindernisse.
    -Einfaches hinzufügen neuer Types
    -Einfach anpassbar über settings

--- obstacle.py
+++ obstacle.py
@@ -2,13 +2,20 @@ import pygame as pg
 from settings import *
 
 class Obstacle(pg.sprite.Sprite):
-    def __init__(self, game, x: float, lane: int):
+    def __init__(self, game, x: float, lane: int, obstacle_type: int):
         self.groups = game.all_sprites, game.obstacles
         super().__init__(self.groups)
+        
         self.game = game
-        self.image = pg.Surface((OBSTACLE_WIDTH, OBSTACLE_HEIGHT))
+        self.obstacle_type = obstacle_type        #Obstacle Typ speichern
+
+        obstacle_width = obstacle_type * OBSTACLE_BASE_WIDTH
+        
+        
+        self.image = pg.Surface((obstacle_width, OBSTACLE_HEIGHT))
         self.image.fill(GRAY)
-        y = lane * LANE_HEIGHT + LANE_HEIGHT // 2 # Position in der Bahn berechnen
+        
+        y = lane * LANE_HEIGHT + LANE_HEIGHT // 2               # Position in der Bahn berechnen
         self.rect = self.image.get_rect(center=(int(x), y))
         
     def update(self, dt: float):
